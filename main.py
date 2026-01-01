358edab399be3a238b1f77e9f11723046adf6ae3
MuhammadAqill <maqil5697@gmail.com>
2026-01-01 23:57:25 +0800

    realtime speak to text

--- main.py
+++ main.py
@@ -3,14 +3,23 @@ from scipy.io.wavfile import write
 import keyboard
 import time
 
+from openai import OpenAI
+
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
 def record_audio():
+
     print("Press Enter to start recording...")
     keyboard.wait('enter') # Wait for the first Enter key press
 
     start_time = time.time()
 
     fs = 44100 # Sample rate
-    channels = 2 # Number of channels
+    channels = 2# Number of channels
 
     # Start recording with an arbitrary large buffer
     recording = sd.rec(int(fs * 300), samplerate=fs, channels=channels)
@@ -27,5 +36,28 @@ def record_audio():
     # Save only teh recorded portion
     write("output.wav", fs, recording[:int(duration * fs)])
 
+def speech_to_text():
+
+    audio_file= open("output", "rb")
+
+    transcription = client.audio.transcriptions.create(
+        model="gpt-4o-transcribe", 
+        file=audio_file
+    )
+
+    return transcription.text
+
+def transcribe():
+
+    while True:
+        record_audio()
+
+        output = speech_to_text()
+
+        print()
+        print('Transcription')
+        print(output)
+        print()
+
 if __name__ == "__main__":
-    record_audio()
+    transcribe()
